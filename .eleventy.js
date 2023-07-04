b12d4abcc306770dd8dcfd0522550b8ec6879141
Tolga Egilmezel <tolga@egilmezel.com>
2023-07-04 07:49:41 +0300

    Added External IP setting

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("img");
-    // eleventyConfig.addPassthroughCopy("**/*.jpg");
-  };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("img");
+  // eleventyConfig.addPassthroughCopy("**/*.jpg");
+
+  eleventyConfig.setServerOptions({
+    showAllHosts: true
+  })
+};
